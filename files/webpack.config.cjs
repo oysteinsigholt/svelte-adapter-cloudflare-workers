b1b6c95f9c715c98a08bbfc7ec6d0d298432187a
oysteinsigholt <oystein.sigholt@gmail.com>
2021-02-12 21:26:56 +0100

    use URLSearchParams from dom

--- files/webpack.config.cjs
+++ files/webpack.config.cjs
@@ -1,13 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    target: "webworker",
-    entry: "./index.js",
-    context: __dirname,
-    mode: "production",
-    resolve: {
-        alias: {
-            '@sveltejs/kit/renderer': path.resolve(__dirname, 'node_modules/@sveltejs/kit/dist/renderer.js')
+  target: 'webworker',
+  entry: './index.js',
+  context: __dirname,
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /renderer\.js$/,
+        loader: 'string-replace-loader',
+        options: {
+          search: "import Url, { parse, resolve, URLSearchParams } from 'url';",
+          replace: "import Url, { parse, resolve } from 'url';",
         },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@sveltejs/kit/renderer': path.resolve(
+        __dirname,
+        'node_modules/@sveltejs/kit/dist/renderer.js'
+      ),
     },
+  },
 };
